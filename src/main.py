e426c219120c78f60cee36fc5f861878cfdcce3b
Samuel <patriciosamuel097@gmail.com>
2025-06-05 20:10:05 -0300

    opencv libraries and cam videocapture

--- src/main.py
+++ src/main.py
@@ -1,4 +1,47 @@
-import numpy as np
+import cv2
 
-# simple code to run and test package
-print(np.round(2.5, 1))
+# Open the default camera
+cam = cv2.VideoCapture(0)
+
+# Get the default frame width and height
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+
+# Define rectangle positions
+x0, y0 = 200, 300
+x1, y1 = 300, 400
+
+# Text formatting
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+if not cam.isOpened():
+    print("Error: Could not open video source.")
+    exit()
+
+while True:
+    ret, frame = cam.read()
+
+    # Write the frame to the output file
+    out.write(frame)
+
+    # face crop
+    frame_rect = cv2.rectangle(frame, (x0, y0), (x1, y1), color=(0, 255, 0), thickness=2)
+    
+    # face text
+    cv2.putText(frame,'happy',(x0, y1 + 20), fontFace=font, fontScale=1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+    
+    # Display the captured frame
+    cv2.imshow('Camera', frame)
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the capture and writer objects
+cam.release()
+out.release()
+cv2.destroyAllWindows()
